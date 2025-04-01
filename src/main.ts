a6d349f00c08a7f196136271deba7529e2790922
christian-2009 <c-sophocleous@hotmail.com>
2025-04-01 20:12:42 +0100

    Add functionality

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { prHelperExtension } from './modules';
-import { PR_FILES_URL_REGEX } from './constants';
+import { COMMENTS_SELECTOR, PR_FILES_URL_REGEX, REACTION_SELECTOR } from './constants';
 
 const observeUrlChange = (isInitialLoad: boolean) => {
   let oldHref = document.location.href;
@@ -32,11 +32,26 @@ const observeUrlChange = (isInitialLoad: boolean) => {
 const observeCommentsChange = () => {
   const commentsChangeContainer = document.querySelector('.js-diff-container');
   const observer = new MutationObserver((mutations) => {
-
     mutations.forEach(mutation => {
       mutation.removedNodes.forEach((node) => {
-        if (node instanceof HTMLElement && node.querySelector('.js-inline-comments-container')) {
-          setTimeout(() => prHelperExtension(), 1000);
+        // when emoji reaction removed
+        if (node instanceof HTMLElement && node.querySelector(REACTION_SELECTOR)) {
+          prHelperExtension();
+        }
+        // when comment resolved
+        if (node instanceof HTMLElement && node.querySelector(COMMENTS_SELECTOR)) {
+          prHelperExtension();
+        }
+      });
+
+      mutation.addedNodes.forEach((node) => {
+        // when emoji is added
+        if (node instanceof HTMLElement && node.querySelector(REACTION_SELECTOR)) {
+          prHelperExtension();
+        }
+        // when comment added
+        if (node instanceof HTMLElement && node.querySelector(COMMENTS_SELECTOR)) {
+          prHelperExtension();
         }
       });
     });
