8c1263d0408ff8a1b8f1c21cfecb0f5008d1c10f
christian-2009 <c-sophocleous@hotmail.com>
2025-04-02 21:42:44 +0100

    Rename numberActionedComments

--- src/modules/renderer.ts
+++ src/modules/renderer.ts
@@ -1,25 +1,25 @@
 import { ACTIONED_COMMENTS_CLASS, PR_HEADER_CONTAINER_SELECTOR } from '../constants';
 
 export const renderCommentInfo = (
-  numberActionedComments: number,
+  numberOfActionedComments: number,
   totalNumberOfComments: number
 ) => {
   const currentActionedCommentsElement = document.querySelector('.comments-actioned') ?? null;
 
   if (!currentActionedCommentsElement) {
-    renderInitialActionedComments(numberActionedComments, totalNumberOfComments);
+    renderInitialActionedComments(numberOfActionedComments, totalNumberOfComments);
   } else {
-    currentActionedCommentsElement.textContent = `${numberActionedComments}/${totalNumberOfComments} have been actioned`;
+    currentActionedCommentsElement.textContent = `${numberOfActionedComments}/${totalNumberOfComments} have been actioned`;
   }
 };
 
 const renderInitialActionedComments = (
-  numberActionedComments: number,
+  numberOfActionedComments: number,
   totalNumberOfComments: number
 ) => {
   const headerContainer = document.querySelector(PR_HEADER_CONTAINER_SELECTOR);
   const actionedCommentsElement = document.createElement('div');
   actionedCommentsElement.classList.add(ACTIONED_COMMENTS_CLASS);
-  actionedCommentsElement.textContent = `${numberActionedComments}/${totalNumberOfComments} have been actioned`;
+  actionedCommentsElement.textContent = `${numberOfActionedComments}/${totalNumberOfComments} have been actioned`;
   headerContainer?.appendChild(actionedCommentsElement);
 };
