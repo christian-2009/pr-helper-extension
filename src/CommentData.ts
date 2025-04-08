80e97a20b17689588c390325fb26045ba84fbb2c
christian-2009 <85346878+christian-2009@users.noreply.github.com>
2025-04-08 20:34:27 +0100

    Add comment description and filename (#9)

--- src/CommentData.ts
+++ src/CommentData.ts
@@ -2,9 +2,13 @@ import { ASSIGNEE_SELECTOR, REACTION_SELECTOR } from './constants';
 
 export class CommentData {
   hasBeenActioned: boolean;
+  commentDescription: string;
+  fileName: string;
 
   constructor(public commentElement: Element, public assignee: string) {
     this.hasBeenActioned = this.hasReaction();
+    this.commentDescription = this.getCommentDescription();
+    this.fileName = this.getFileName();
   }
 
   isLastReplyFromAssignee = () => {
@@ -20,7 +24,19 @@ export class CommentData {
 
   hasReaction = () => !!this.commentElement.querySelector(REACTION_SELECTOR)?.textContent;
 
-  getCommentTitle = (commentElement: HTMLElement): string => {
-    return 'harry potter';
+  getCommentDescription = (): string =>
+    this.commentElement.querySelectorAll('.js-comment-body')[0]?.textContent?.trim() ?? '';
+
+  getFileName = (): string => {
+    const fileElementsInPr = document.querySelectorAll('.js-file.js-details-container');
+    let fileElementCommentIsIn;
+
+    for (const file of fileElementsInPr) {
+      if (file.contains(this.commentElement)) {
+        fileElementCommentIsIn = file;
+      }
+    }
+
+    return fileElementCommentIsIn?.querySelector('.Link--primary.Truncate-text')?.textContent ?? '';
   };
 }
